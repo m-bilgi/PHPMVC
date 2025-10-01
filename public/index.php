72c5930c8796cf4c2f675a2db82356338067b1bb
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-01 20:10:58 +0300

    Create index.php
    Initial commit

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Core\Router;
+use Core\Request;
+
+session_start();
+
+// Creating a router instance and filling it with the routes file
+$router = new Router();
+require_once __DIR__ . '/../routes.php';
+
+// Dispatch - controller is called based on the incoming URI and method
+try {
+    $router->dispatch(Request::uri(), Request::method());
+} catch (Throwable $e) {
+    http_response_code(500);
+    echo "500 - Internal Server Error<br>";
+    echo htmlspecialchars($e->getMessage());
+}
