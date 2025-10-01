5a805f6e4ded7b1aec1279978023a71aa65d3426
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-01 21:15:04 +0300

    Update routes.php
    Layout update

--- routes.php
+++ routes.php
@@ -1,9 +1,14 @@
 <?php
 // The $router object is created and included in public/index.php.
 $router->get('/', 'HomeController@index');
+
+// Without layout
+$router->get('/render', 'HomeController@render');
+
 $router->get('/hello', function () {
     return 'Hello! Closure and route worked.';
 });
 
 // Parameterized route example
+// For numeric value only: {id:\d+}
 $router->get('/user/{id}', 'HomeController@show');
