65fb7c4a57507d8c06d590f35b0a8de60133597b
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-01 21:08:32 +0300

    Update View.php
    Add renderWithLayout method

--- core/View.php
+++ core/View.php
@@ -9,9 +9,25 @@ class View
         if (!file_exists($full)) {
             throw new \Exception("View not found: " . $full);
         }
+
         extract($params, EXTR_OVERWRITE);
         ob_start();
         include $full;
+        return ob_get_clean();
+    }
+
+    public static function renderWithLayout(string $viewPath, string $layoutPath, array $params = []): string
+    {
+        $content = self::render($viewPath, $params);
+
+        $fullLayout = __DIR__ . '/../app/Views/' . ltrim($layoutPath, '/');
+        if (!file_exists($fullLayout)) {
+            throw new \Exception("Layout not found: " . $fullLayout);
+        }
+
+        extract($params, EXTR_OVERWRITE);
+        ob_start();
+        include $fullLayout;
         return ob_get_clean();
     }
 }
