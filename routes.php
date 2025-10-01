9c6d94a66dec7023b00f3e25bc5b67f384df08d0
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-01 20:14:30 +0300

    Create routes.php
    Initial commit

---
+++ routes.php
@@ -0,0 +1,6 @@
+<?php
+// The $router object is created and included in public/index.php.
+$router->get('/', 'HomeController@index');
+$router->get('/hello', function () {
+    return 'Hello! Closure and route worked.';
+});
