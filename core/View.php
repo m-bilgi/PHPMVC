02f60ad9c8f5e5c64d8d2485a3b9cda5487b583f
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-02 16:53:48 +0300

    Update View.php
    Section, Yield and Include System

--- core/View.php
+++ core/View.php
@@ -3,6 +3,9 @@ namespace Core;
 
 class View
 {
+    protected static array $sections = [];
+    protected static array $sectionStack = [];
+
     public static function render(string $viewPath, array $params = []): string
     {
         $full = __DIR__ . '/../app/Views/' . ltrim($viewPath, '/');
@@ -18,8 +21,15 @@ class View
 
     public static function renderWithLayout(string $viewPath, string $layoutPath, array $params = []): string
     {
+        // Render the content (sections will also be filled in during this time)
         $content = self::render($viewPath, $params);
 
+        // Default content section
+        if (!isset(self::$sections['content'])) {
+            self::$sections['content'] = $content;
+        }
+
+        // Render layout
         $fullLayout = __DIR__ . '/../app/Views/' . ltrim($layoutPath, '/');
         if (!file_exists($fullLayout)) {
             throw new \Exception("Layout not found: " . $fullLayout);
@@ -28,6 +38,37 @@ class View
         extract($params, EXTR_OVERWRITE);
         ob_start();
         include $fullLayout;
-        return ob_get_clean();
+        $output = ob_get_clean();
+
+        // Cleaning
+        self::$sections = [];
+        self::$sectionStack = [];
+
+        return $output;
+    }
+
+    // === Section / Yield System ===
+
+    public static function startSection(string $name): void
+    {
+        self::$sectionStack[] = $name;
+        ob_start();
+    }
+
+    public static function endSection(): void
+    {
+        $name = array_pop(self::$sectionStack);
+        self::$sections[$name] = ob_get_clean();
+    }
+
+    public static function yield(string $name, string $default = ''): void
+    {
+        echo self::$sections[$name] ?? $default;
+    }
+
+    // === Include System ===
+    public static function include(string $viewPath, array $params = []): void
+    {
+        echo self::render($viewPath, $params);
     }
 }
