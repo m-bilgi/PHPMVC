ad18aa00d2bdb66e01fae172dc24b7a43adcf216
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-02 19:02:59 +0300

    Update index.php
    Add yieldContentOr (callback fallback) function

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -6,3 +6,10 @@
     <p>Hello, <?= htmlspecialchars($user) ?>.</p>
     <p>Home page content...</p>
 <?php endsection(); ?>
+
+<?php section('sidebar'); ?>
+<ul>
+    <li>Nav 1</li>
+    <li>Nav 2</li>
+</ul>
+<?php endsection(); ?>
