2c5ce8e1ef43771d9f6369a2cb7a947dad9f6b8a
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-09 18:20:09 +0300

    Update header.php

--- app/Views/partials/header.php
+++ app/Views/partials/header.php
@@ -1,6 +1,5 @@
 <h1><a href="/">Site Header</a></h1>
 <nav>
-    <a href="/render">Render view (without layout)</a> | 
-    <a href="/hello">Hello (Callback)</a> | 
-    <a href="/user/1">Url Parameter</a>
+    <a href="/categories">Category</a> | 
+    <a href="/members">Members</a>
 </nav>
