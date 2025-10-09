dbdfe083c394ef33239c786108e13b7072424311
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-09 18:13:58 +0300

    Update index.php

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -4,12 +4,15 @@
 
 <?php section('content'); ?>
     <p>Hello, <?= htmlspecialchars($user) ?>.</p>
-    <p>Home page content...</p>
+    <p>Home page content was rendered with layout..</p>
 <?php endsection(); ?>
+
+    <p>Hello, <?= htmlspecialchars($user) ?>.</p>
+    <p>This page content was rendered Without layout..</p>
 
 <?php section('sidebar'); ?>
 <ul>
-    <li>Nav 1</li>
+    <li><a href="/category">Category</a></li>
     <li>Nav 2</li>
 </ul>
 <?php endsection(); ?>
