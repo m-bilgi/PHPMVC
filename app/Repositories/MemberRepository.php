db26fccfee7dc48a34e1b55b7c84ea3c8cd053c2
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-09 15:55:01 +0300

    Update MemberRepository.php

--- app/Repositories/MemberRepository.php
+++ app/Repositories/MemberRepository.php
@@ -22,13 +22,10 @@ class MemberRepository
         $stmt->bindValue(':anyValue01', $options->anyValue01);
         $stmt->bindValue(':anyValue02', $options->anyValue02);
         $stmt->bindValue(':userId', $model->id, PDO::PARAM_INT);
-        $stmt->bindValue(':userGuid', $model->guid);
         $stmt->bindValue(':userEmail', $model->email);
         $stmt->bindValue(':username', $model->username);
         $stmt->bindValue(':userPassword', $model->password);
         $stmt->bindValue(':userLevel', $model->level, PDO::PARAM_INT);
-        $stmt->bindValue(':userActivationKey', $model->activation_key);
-        $stmt->bindValue(':userRegisterDate', $model->register_date);
     }
 
     /**
@@ -111,7 +108,7 @@ class MemberRepository
     public function insert(QueryOptions $options, Member $model): bool
     {
         try {
-            $stmt = $this->pdo->prepare('CALL sp_insert_member(:procType, :userFirstname, :userSurname, :userEmail, :username, :userPassword, :userLevel, :userActivationKey, :userAvatarUrl, :userBirthday, :userCountryId, :userGender, :userIp, :userLanguage, :userRegisterDate, :userStatus, :userTheme, :userKey2)');
+            $stmt = $this->pdo->prepare('CALL sp_insert_member(:procType, :userFirstname, :userSurname, :userEmail, :username, :userPassword, :userLevel, :userStatus)');
             $stmt->bindValue(':procType', $options->procType);
             $stmt->bindValue(':userFirstname', $model->name);
             $stmt->bindValue(':userSurname', $model->surname);
@@ -119,19 +116,7 @@ class MemberRepository
             $stmt->bindValue(':username', $model->username);
             $stmt->bindValue(':userPassword', $model->password);
             $stmt->bindValue(':userLevel', $model->level, PDO::PARAM_INT);
-            $stmt->bindValue(':userActivationKey', $model->activation_key);
-            $stmt->bindValue(':userAvatarUrl', $model->avatar_url);
-            $stmt->bindValue(':userBirthday', $model->birthday);
-            $stmt->bindValue(':userCountryId', $model->country_id, PDO::PARAM_INT);
-            $stmt->bindValue(':userGender', $model->gender);
-            $stmt->bindValue(':userIp', $model->ip_address);
-            $stmt->bindValue(':userLanguage', $model->language);
-            //$stmt->bindValue(':userLastIp', $model->last_ip_address);
-            //$stmt->bindValue(':userLastHereDate', $model->last_here_date);
-            $stmt->bindValue(':userRegisterDate', $model->register_date);
             $stmt->bindValue(':userStatus', $model->status, PDO::PARAM_INT);
-            $stmt->bindValue(':userTheme', $model->theme);
-            $stmt->bindValue(':userKey2', $model->key2);
 
             $result = $stmt->execute();
             $stmt->closeCursor();
@@ -158,56 +143,16 @@ class MemberRepository
     public function update(QueryOptions $options, Member $model): bool
     {
         try {
-            $stmt = $this->pdo->prepare("CALL sp_update_member(:procType, :userId, :userGuid, :userFirstname, :userSurname, :userEmail, :username, :userPassword, :userLevel, :userAvatarUrl, :userBiography, :userBirthday, :userCity, :userCountryId, :userDescription, :userGender, :userHideBirthday, :userHideEmail, :userHideFullname, :userHideOnline, :userHobbies, :userHomepage, :userIp, :userLanguage, :userLastHereDate, :userLastPostDate, :userNewEmail, :userOccupation, :userPageViews, :userPmEmail, :userPmReceive, :userPosts, :userQuote, :userReceiveEmail, :userReply, :userReplyTotal, :userSignature, :userSocialMedia, :userState, :userStatus, :userSubscription, :userTheme, :userTitle)");
+            $stmt = $this->pdo->prepare("CALL sp_update_member(:procType, :userId, :userFirstname, :userSurname, :userEmail, :username, :userPassword, :userLevel, :userTheme)");
             $stmt->bindValue(':procType', $options->procType);
             $stmt->bindValue(':userId', $model->id, PDO::PARAM_INT);
-            $stmt->bindValue(':userGuid', $model->guid);
             $stmt->bindValue(':userFirstname', $model->name);
             $stmt->bindValue(':userSurname', $model->surname);
             $stmt->bindValue(':userEmail', $model->email);
             $stmt->bindValue(':username', $model->username);
             $stmt->bindValue(':userPassword', $model->password);
             $stmt->bindValue(':userLevel', $model->level, PDO::PARAM_INT);
-            //$stmt->bindValue(':userActivationKey', $model->activation_key);
-            $stmt->bindValue(':userAvatarUrl', $model->avatar_url);
-            $stmt->bindValue(':userBiography', $model->biography);
-            $stmt->bindValue(':userBirthday', $model->birthday);
-            $stmt->bindValue(':userCity', $model->city);
-            $stmt->bindValue(':userCountryId', $model->country_id, PDO::PARAM_INT);
-            $stmt->bindValue(':userDescription', $model->description);
-            $stmt->bindValue(':userGender', $model->gender);
-            $stmt->bindValue(':userHideBirthday', $model->hide_birthday, PDO::PARAM_INT);
-            $stmt->bindValue(':userHideEmail', $model->hide_email, PDO::PARAM_INT);
-            $stmt->bindValue(':userHideFullname', $model->hide_fullname, PDO::PARAM_INT);
-            $stmt->bindValue(':userHideOnline', $model->hide_online, PDO::PARAM_INT);
-            $stmt->bindValue(':userHobbies', $model->hobbies);
-            $stmt->bindValue(':userHomepage', $model->homepage);
-            $stmt->bindValue(':userIp', $model->ip_address);
-            $stmt->bindValue(':userLanguage', $model->language);
-            //$stmt->bindValue(':userLastIp', $model->last_ip_address);
-            $stmt->bindValue(':userLastHereDate', $model->last_here_date);
-            $stmt->bindValue(':userLastPostDate', $model->last_post_date);
-            $stmt->bindValue(':userNewEmail', $model->new_email);
-            $stmt->bindValue(':userOccupation', $model->occupation);
-            $stmt->bindValue(':userPageViews', $model->page_views, PDO::PARAM_INT);
-            //$stmt->bindValue(':userPhotoUrl', $model->photo_url);
-            $stmt->bindValue(':userPmEmail', $model->pm_email, PDO::PARAM_INT);
-            $stmt->bindValue(':userPmReceive', $model->pm_receive, PDO::PARAM_INT);
-            $stmt->bindValue(':userPosts', $model->posts, PDO::PARAM_INT);
-            $stmt->bindValue(':userQuote', $model->quote);
-            $stmt->bindValue(':userReceiveEmail', $model->receive_email, PDO::PARAM_INT);
-            //$stmt->bindValue(':userRecMail', $model->recmail, PDO::PARAM_INT);
-            //$stmt->bindValue(':userRegisterDate', $model->register_date);
-            $stmt->bindValue(':userReply', $model->reply, PDO::PARAM_INT);
-            $stmt->bindValue(':userReplyTotal', $model->reply_total, PDO::PARAM_INT);
-            $stmt->bindValue(':userSignature', $model->signature);
-            $stmt->bindValue(':userSocialMedia', $model->social_media);
-            $stmt->bindValue(':userState', $model->state);
             $stmt->bindValue(':userStatus', $model->status, PDO::PARAM_INT);
-            $stmt->bindValue(':userSubscription', $model->subscription, PDO::PARAM_INT);
-            $stmt->bindValue(':userTheme', $model->theme);
-            $stmt->bindValue(':userTitle', $model->title);
-            //$stmt->bindValue(':userKey2', $model->key2);
 
             $result = $stmt->execute();
             $stmt->closeCursor();
