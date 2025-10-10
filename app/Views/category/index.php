907afc7b8ccc39314fac4cd99980fd1d6bcee0e2
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-10 14:57:50 +0300

    Create index.php
    Initial commit

---
+++ app/Views/category/index.php
@@ -0,0 +1,21 @@
+<?php section('title'); ?>
+    Category Page
+<?php endsection(); ?>
+
+<?php
+section('content');
+    echo '<ul>';
+        foreach ($dataList as $c) {
+            echo '<li class="flex space-y-1">';
+            if ($c->status === 1) {
+                echo '<a href="/category/show/'. $c->url .'" class="hover:underline font-bold">'. $c->name .'</a>';
+            } else {
+                echo '<a href="/category/show/'. $c->url .'" class="hover:underline text-gray-400 line-through font-bold">'. $c->name .'</a>';
+            }
+            echo '</li>';
+        }
+    echo '</ul>';
+endsection();
+?>
+
+<?= includeFile('/category/partials/sidebar.php'); ?>
