66bbfbc01ad04070a35f20b66b312c346b42a1d0
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-10 14:57:12 +0300

    Create delete.php
    Initial commit

---
+++ app/Views/category/delete.php
@@ -0,0 +1,16 @@
+<?php section('title'); ?>
+    <?= $data->name ?>
+<?php endsection(); ?>
+
+<?php section('content'); ?>
+    <h2 class="flex pb-5 gap-2"><?= $data->name ?></h2>
+    <p><p>Are you sure you want to delete this content?</p></p>
+    <form action="/category/delete-post" method="POST">
+        <?= csrf_field() ?>
+        <input type="hidden" name="sig" value="<?= htmlspecialchars($data->signature) ?>">
+        <input type="hidden" name="id" value="<?= $data->id ?>">
+        <button type="submit" class="mt-4 px-2 border border-red-600 bg-red-500 hover:bg-red-700 text-white">Delete</button>
+    </form>
+<?php endsection(); ?>
+
+<?= includeFile('/category/partials/sidebar.php'); ?>
