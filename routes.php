b5b92dd67e608285f4489a89278eac836b1094c3
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-10 15:02:43 +0300

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,14 +1,25 @@
 <?php
-// The $router object is created and included in public/index.php.
+// $The $router object is created and included in public/index.php.
 $router->get('/', 'HomeController@index');
 
 // Without layout
 $router->get('/render', 'HomeController@render');
 
 $router->get('/hello', function () {
-    return 'Hello! Closure and route worked.';
+    return '<p>Hello!</p><p>This page was rendered without layout.</p>Closure and route worked.';
 });
 
 // Parameterized route example
 // For numeric value only: {id:\d+}
 $router->get('/user/{id}', 'HomeController@show');
+
+// Category Pages
+$router->get('/category', 'CategoryController@index');
+$router->get('/category/show/{slug}', 'CategoryController@show');
+$router->get('/category/edit/{slug}', 'CategoryController@edit');
+$router->get('/category/insert', 'CategoryController@insert');
+$router->get('/category/delete/{slug}', 'CategoryController@delete');
+
+$router->post('/category/edit-post', 'CategoryController@editPost');
+$router->post('/category/insert-post', 'CategoryController@insertPost');
+$router->post('/category/delete-post', 'CategoryController@deletePost');
