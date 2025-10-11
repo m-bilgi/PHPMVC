f4bbfcccf45165bbb371566621f19a9860836418
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-11 13:01:44 +0300

    Update edit.php

--- app/Views/category/edit.php
+++ app/Views/category/edit.php
@@ -1,4 +1,4 @@
-<?php section('title', 'Edit page: ' . $data->name); ?>
+<?php section('title', htmlspecialchars($title)); ?>
 
 <?php section('content'); ?>
     <h2 class="flex pb-5 gap-2"><?= $data->name ?></h2>
