3280d71d73b2a8e0e64db2ed87b6db9e63be8ca6
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-17 10:53:47 +0300

    Update Request.php
    optional parameter system update

--- core/Request.php
+++ core/Request.php
@@ -6,8 +6,7 @@ class Request
     public static function uri(): string
     {
         $uri = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH);
-        // If you want to remove the base path if index.php is in a subdirectory, add logic here.
-        return rtrim($uri, '/') === '' ? '/' : rtrim($uri, '/');
+        return $uri ?: '/';
     }
 
     public static function method(): string
