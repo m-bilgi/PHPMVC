32ae3055f121bbd4e76910c716e31cf748c9a5e1
Mustafa Bilgi <99275373+m-bilgi@users.noreply.github.com>
2025-10-17 12:52:58 +0300

    Update HomeController.php
    added include partial

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -38,4 +38,14 @@ class HomeController
                 '<p>PageNo: '. $pageNo .'</p>' .
                 '<a href="/show/articles/1">1</a> | <a href="/show/articles/2">2</a> | <a href="/show/articles/3">3</a> | <a href="/show/articles/4">4</a> | <a href="/show/articles/5">5</a>';
     }
+
+    // Partial
+    public function partialData()
+    {
+        $data = [
+            'title' => 'Include partial content',
+            'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+            ];
+        return $data;
+    }
 }
